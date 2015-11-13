54e1bdf9d142621e20041f28ee50cbf4cdce5ea3
Ash Dehghan <ash.dehghan@gmail.com>
2015-11-13 11:57:08 -0500

    From Ash: I fixed up the basics of the main two activities. The front (main) activity, I only added the Add Alarm button, to take you to the next activity which is AddAlarm activity. There I added some basic layout. Everything works so far, but no data is being saved, everything works on surafce.

--- gen/com/kash/alarmtag/R.java
+++ gen/com/kash/alarmtag/R.java
@@ -25,18 +25,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int button_addAlarm=0x7f080000;
+        public static final int editText_alarmName=0x7f080002;
+        public static final int textView_title=0x7f080001;
+        public static final int timePicker=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_set_alarm=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int set_alarm=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_set_alarm=0x7f050003;
     }
     public static final class style {
         /** 
--- src/com/kash/alarmtag/SetAlarm.java
+++ src/com/kash/alarmtag/SetAlarm.java
@@ -3,26 +3,32 @@ package com.kash.alarmtag;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class SetAlarm extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
+		setContentView(R.layout.activity_set_alarm);
+		
+		
+		
 		Button addAlarm = (Button) findViewById(R.id.button_addAlarm);
 		// When the login button is clicked this will be trigered
 		addAlarm.setOnClickListener(new Button.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, SetAlarm.class);
+				Intent intent = new Intent(SetAlarm.this, MainActivity.class);
 				startActivity(intent);
 			}
 		});
-
+		
+		
 	}
+
 
 }
